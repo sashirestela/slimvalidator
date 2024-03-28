ffc484ebda3b8441d9ffaf42ff28ce10a60a7475
Sashir Estela <sashirestela@yahoo.com>
2024-03-28 00:39:30 +0000

    Fix code smell in unit test

--- src/test/java/io/github/sashirestela/slimvalidator/validators/RequiredValidatorTest.java
+++ src/test/java/io/github/sashirestela/slimvalidator/validators/RequiredValidatorTest.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RequiredValidatorTest {
+class RequiredValidatorTest {
 
     @Test
     void shouldReturnExpectedResultWhenEvaluatingDataPassed() {
