25699745016fcc78be906ac33f94fae5c1c1b257
Sashir Estela <sashirestela@yahoo.com>
2024-04-06 00:11:17 +0000

    Add Documented annotation

--- src/main/java/io/github/sashirestela/slimvalidator/constraints/Extension.java
+++ src/main/java/io/github/sashirestela/slimvalidator/constraints/Extension.java
@@ -3,11 +3,13 @@ package io.github.sashirestela.slimvalidator.constraints;
 import io.github.sashirestela.slimvalidator.Constraint;
 import io.github.sashirestela.slimvalidator.validators.ExtensionValidator;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Documented
 @Constraint(validatedBy = ExtensionValidator.class)
 @Target({ ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
